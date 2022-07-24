c855e859e722ce769f532c2ed51f17f452cba371
2439905184 <suchenglong018@126.com>
2022-07-24 20:34:49 +0800

    ✨ feat: 通过调用exe实现下载aar

--- Global.gd
+++ Global.gd
@@ -4,11 +4,11 @@ extends Node
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
+var exe_dir
 var editor_dir
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var exe_dir = OS.get_executable_path().get_base_dir()
+	exe_dir = OS.get_executable_path().get_base_dir()
 	editor_dir = exe_dir + "/editor"
 	pass # Replace with function body.
 
--- aar_line.gd
+++ aar_line.gd
@@ -1,21 +1,21 @@
 extends HBoxContainer
 var downloaded_size = ""
 func _ready():
-	$HTTPRequest.connect("request_completed",self,"_on_download_completed")
+	#$HTTPRequest.connect("request_completed",self,"_on_download_completed")
 	pass
 	
 func _process(delta):
 	var downloaded_bytes = $HTTPRequest.get_downloaded_bytes()
 	downloaded_size = str(downloaded_bytes).humanize_size(downloaded_bytes)
-	$status.text = "下载中" + downloaded_size
+	#$status.text = "下载中" + downloaded_size
 	pass
 	
 func _on_download_pressed(url):
+	print_debug("开始下载！")
 	print_debug(url)
-	$HTTPRequest.request(url)
-	var download_path = Global.editor_dir + "/" + url.get_file()
-	$HTTPRequest.download_file = download_path
-	print_debug(download_path)
+	var output = []
+	OS.execute(Global.exe_dir + "/downloader.exe",[url],false,output)
+	print_debug(output)
 	pass
 
 func _on_download_completed(result, response_code, headers, body):
