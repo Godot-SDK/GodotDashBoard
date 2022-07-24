a140bdaa8f71c0a5e16915d14363cae8dce9c97d
2439905184 <suchenglong018@126.com>
2022-07-24 18:40:44 +0800

    同步

---
+++ Global.gd
@@ -0,0 +1,18 @@
+extends Node
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+var editor_dir
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var exe_dir = OS.get_executable_path().get_base_dir()
+	editor_dir = exe_dir + "/editor"
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
--- line.gd
+++ line.gd
@@ -1,12 +1,35 @@
+tool
 extends HBoxContainer
 
+#这个脚本包含了测试代码，需要整理
+var downloaded_size = ""
 func _ready():
+	#var a = "https://downloads.tuxfamily.org/godotengine/3.0/Godot_v3.0-stable_win32.exe.zip"
+	#$HTTPRequest.request("https://downloads.tuxfamily.org/godotengine/3.0/Godot_v3.0-stable_win32.exe.zip")
+	$HTTPRequest.connect("request_completed",self,"_on_get_completed")
+	$status.text = "下载中"
+	#$HTTPRequest.download_file = Global.editor_dir + "/test.exe.zip"
 	pass 
+
+func _process(delta):
+	var downloaded_bytes = $HTTPRequest.get_downloaded_bytes()
+	downloaded_size = str(downloaded_bytes).humanize_size(downloaded_bytes)
+	$status.text = "下载中" + downloaded_size
+	#print_debug($HTTPRequest.get_downloaded_bytes())
+	pass
 func _on_button_32_pressed(url):
-	
 	print_debug(url)
+	$HTTPRequest.request(url)
+	$HTTPRequest.download_file = Global.editor_dir + "/" + url.get_file()
+	
 	pass
 func _on_button_64_pressed(url):
-	
 	print_debug(url)
+	$HTTPRequest.request(url)
 	pass
+
+func _on_get_completed(result, response_code, headers, body):
+	print_debug(response_code)
+	print_debug(body)
+	pass
+	
